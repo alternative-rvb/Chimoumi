035c863caece2fffd83f2bfa847b9e9ca46ddee2
Nicolas <alternative.rvb@gmail.com>
2023-10-28 03:15:52 +0200

    Modif responsive topnav

--- js/topnav.js
+++ js/topnav.js
@@ -1,12 +1,23 @@
-const burger = document.querySelector(".arvb-nav__menu-btn");
-const sideBar = document.querySelector(".arvb-nav__sidebar");
+document.addEventListener("click", (e) => {
+  const btnToWatch = e.target.closest(".arvb-nav__menu-btn");
+  console.log("btnToWatch:", btnToWatch);
 
-burger.addEventListener("click", faireQC);
+  const containerToWatch = e.target.closest(".open");
+  console.log("containerToWatch:", containerToWatch);
 
-function faireQC(e) {
-  console.log(e.currentTarget.children[0]);
-  if (e.currentTarget.children[0].classList != "bi bi-x-lg")
-    e.currentTarget.children[0].classList = "bi bi-x-lg";
-  else e.currentTarget.children[0].classList = "bi bi-list";
-  sideBar.classList.toggle("open");
-}
+  const sideBarContainer = document.querySelector(".arvb-nav__sidebar");
+  const btn = document.querySelector(".arvb-nav__menu-btn i");
+
+  if (btnToWatch) {
+    sideBarContainer.classList.toggle("open");
+
+    btn.classList != "bi bi-x-lg"
+      ? (btn.classList = "bi bi-x-lg")
+      : (btn.classList = "bi bi-list");
+  }
+
+  if (!containerToWatch && !btnToWatch) {
+    sideBarContainer.classList.remove("open");
+    btn.classList = "bi bi-list";
+  }
+});
